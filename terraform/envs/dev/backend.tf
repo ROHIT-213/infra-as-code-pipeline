0f12d064c2b931de695bcb6943e116cdb813341e
ROHIT-213 <rs2101396@gmail.com>
2026-05-04 16:26:49 +0530

    updated yaml file

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "infra-pipeline-terraform-state-rohit-213"
-#     key            = "dev/terraform.tfstate"
-#     region         = "ap-south-1"
-#     dynamodb_table = "infra-pipeline-terraform-locks"
-#     encrypt        = true
-#   }
-# }
 terraform {
   backend "s3" {
     bucket         = "infra-pipeline-terraform-state-rohit-213"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
+    dynamodb_table = "infra-pipeline-terraform-locks"
     encrypt        = true
-    use_lockfile   = true
   }
 }
+# terraform {
+#   backend "s3" {
+#     bucket         = "infra-pipeline-terraform-state-rohit-213"
+#     key            = "dev/terraform.tfstate"
+#     region         = "ap-south-1"
+#     encrypt        = true
+#     use_lockfile   = true
+#   }
+# }
--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -1,18 +1,18 @@
-terraform {
-  backend "s3" {
-    bucket       = "infra-pipeline-tf-state"
-    key          = "prod/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-  }
-}
-
 # terraform {
 #   backend "s3" {
-#     bucket         = "infra-pipeline-terraform-state-rohit-213"
-#     key            = "prod/terraform.tfstate"
-#     region         = "ap-south-1"
-#     dynamodb_table = "infra-pipeline-terraform-locks"
-#     encrypt        = true
+#     bucket       = "infra-pipeline-tf-state"
+#     key          = "prod/terraform.tfstate"
+#     region       = "ap-south-1"
+#     use_lockfile = true
 #   }
 # }
+
+terraform {
+  backend "s3" {
+    bucket         = "infra-pipeline-terraform-state-rohit-213"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "infra-pipeline-terraform-locks"
+    encrypt        = true
+  }
+}
