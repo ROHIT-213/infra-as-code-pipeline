773078d9ff6fc8baf0c1b5bb04d166d4388c5df6
ROHIT-213 <rs2101396@gmail.com>
2026-05-05 22:22:49 +0530

    Rollback test4

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -74,6 +74,10 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = var.task_cpu   
   memory                   = var.task_memory          //comes from terraform.tfvars different per environment
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  
+  lifecycle {                                         //line added
+    ignore_changes = [container_definitions]
+  }
 
   container_definitions = jsonencode([{
     name  = "${var.project_name}-${var.environment}"
