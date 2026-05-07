99e7e32ac424b6102c87f9417c9019b01cba4f46
ROHIT-213 <rs2101396@gmail.com>
2026-05-07 06:30:17 +0530

    changed main.tf in compute and ci-cd.yml

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -75,18 +75,14 @@ resource "aws_ecs_task_definition" "app" {
   memory                   = var.task_memory          //comes from terraform.tfvars different per environment
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  lifecycle {
-    create_before_destroy = true
-    # This prevents the previous revision from becoming INACTIVE immediately
-    # which allows ECS to keep running existing tasks until new ones are healthy
-    ignore_changes = [container_definitions]
-    
-  }
-  
-  # lifecycle {                                         //line added
+  # lifecycle {
+  #   create_before_destroy = true
+  #   # This prevents the previous revision from becoming INACTIVE immediately
+  #   # which allows ECS to keep running existing tasks until new ones are healthy
   #   ignore_changes = [container_definitions]
+    
   # }
-
+  
   container_definitions = jsonencode([{
     name  = "${var.project_name}-${var.environment}"
     image = var.container_image                //the ECR image URI passed in by the pipeline
